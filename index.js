43f6597b2d06f4aaa39791f8e50301ef0e72faa4
unknown <kavita.kaur998@gmail.com>
2021-12-06 18:12:47 +0000

    tasks completed

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-
-// console.log(myCollection[0].name)
+import myCollection from "./collection.js"
+import chalk from 'chalk';
 
 
 function describeItem(item) {
@@ -25,13 +8,25 @@ function describeItem(item) {
             let count1 = myCollection[i].count
             let like = myCollection[i].whatILike
             if (count1 > 1){
-            console.log(`I have ${count1} ${item}s. Here's what I like about them: ${like}`)
+            console.log(`I have ${chalk.yellow(count1)} ${chalk.cyan(item)}s. Here's what I like about them: ${chalk.green(like)}`)
             }
             else {
-                console.log(`I have a ${item}. Here's what I like about it: ${like}`)
+                console.log(`I have a ${chalk.cyan(item)}. Here's what I like about it: ${chalk.green(like)}`)
             }
         }
     }
 };
 
-describeItem("School of Code hat")
+// describeItem("School of Code hat")
+
+
+function describeCollection (array) {
+    for (let i=0; i<array.length; i++) {
+        describeItem(myCollection[i].name)
+    }
+}
+
+describeCollection(myCollection)
+
+// part 3
+
